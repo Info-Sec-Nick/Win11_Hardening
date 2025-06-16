1b98871d849807cdfa8cb8e4234062c232b9d0a5
info-sec-nick <nicholaskroepsch@proton.me>
2025-06-16 11:45:00 +0930

    updating

---
+++ Hardening_Compliance_Check.ps1
@@ -0,0 +1,75 @@
+
+
+##############################################################
+# TITLE: Windows System Hardening Tool - ISM Aligned         #
+#                                                            #
+# AUTHOR: Nick Kroepsch                                      #     
+#                                                            #
+# VERSION: Version 0.1                                       #
+#                                                            #
+# DESCRIPTION: This tool will audit the current system and   #
+#              do some basic checks to see if the system     # 
+#              aligns with the ISM framework.                #
+##############################################################
+
+
+
+
+## LIST OF AUDIT CHECKS AGAINST THE SYSTEM ##
+function Check-WindowsDefender {
+    $service = Get-Service -Name WinDefend
+    if ($service.Status -eq 'Running') {
+        return "PASS: Windows Defender is running`nCompliant with ISM Control [ISM-1341] - A HIPS or EDR solution is implemented on Workstations. "
+    } else {
+        return "FAIL: Windows Defender is not running"
+    }
+}
+
+function Check-PowerShellVersion {
+    $currentversion = $PSVersionTable.PSVersion.Major
+    if ($currentversion -gt 2) {
+        return "PASS: PowerShell version is currently $currentversion, running newer than v2.0`nCompliant with ISM Control [ISM-1621] - Windows PowerShell 2.0 is disabled or removed" 
+    } else {
+        return "FAIL: PowerShell currently $currentversion, and is not running newer than v2.0"
+    }
+}
+
+function Check-OperatingVersion {
+    $os = Get-CimInstance -ClassName Win32_OperatingSystem
+    $version = $os.Version
+    $caption = $os.Caption
+
+    if ([version]$version -ge [version]"10.0.19045") {
+        return "PASS: Windows Operating System is running $caption (Version $version), this is the newest up-to-date version.`nCompliant with ISM Control [ISM-1407] - The latest release or previous release of operating systems are used."
+    } else {
+        return "FAIL: Windows Operating System is running $caption (Version $version). Please update your system or contact your system administrator."
+    }
+}
+
+## MAIN FUCNTION TO RUN CHECKS ##
+function Main {
+    Write-Host "Starting ISM Windows Hardening Checks...`n" -ForegroundColor DarkCyan
+
+    $results = @()
+    $results += Check-WindowsDefender
+    $results += Check-PowerShellVersion
+    $results += Check-OperatingVersion
+
+    $results | ForEach-Object { 
+        if ($_ -like "PASS*") {
+            Write-Host $_ -ForegroundColor Green
+        } elseif ($_ -like "FAIL*") {
+            Write-Host $_ -ForegroundColor Red
+        } else {
+            Write-Host $_
+        }
+        Write-Host "" # Blank line
+        
+    }
+
+    $timestamp = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
+    $results | Out-File -FilePath ".\hardening_report_$timestamp.txt"
+
+    Write-Output "`nResults saved to: hardening_report_$timestamp.txt"
+}
+Main
